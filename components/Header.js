450b8a95eea8d0a36179463f617404eeaee8ecad
kanaktyagi <tyagiknk.india@gmail.com>
2022-03-14 22:43:16 +0530

    Added the css for the site

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import styled from '@emotion/styled'
+import Image from 'next/image'
 
-function Header() {
+function Header({isDark}) {
   return (
-    <header> I am Header</header>
+    <HearderStyles isDark={isDark}>
+     <div className='container'>
+       <div className="logo">
+     <Image src="/images/logo.svg" alt="sites logo" width="20" height="20"/>
+     <span className="logo-text">Next Movies</span>
+     </div>
+     
+     </div>
+     </HearderStyles>
   )
 }
+
+const HearderStyles = styled.header`
+background : ${props =>props.isDark ? '#000000' : '#efefef'};
+padding:20px;
+.logo {
+    display: flex;
+    align-items: center;
+    position:relative;
+    .logo-text {
+        color: #333333;
+        font-weight: bold;
+        font-size: 20px;
+        margin-left: 20px;
+    }
+}
+`
 
 export default Header
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,22 @@
 import Header from '../components/Header'
 import '../styles/globals.css'
+import {ThemeProvider} from '@emotion/react'
+import GlobalStyles from '../components/GlobalStyles/GlobalStyles'
+
+const theme = {
+  colors: {
+    primary : 'lightgray'
+  }
+}
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-   <Header></Header>
+    <ThemeProvider theme={theme}>
+    <GlobalStyles/>
+   <Header ></Header>
   <Component {...pageProps} />
+  </ThemeProvider>
   </>
   )
 }
