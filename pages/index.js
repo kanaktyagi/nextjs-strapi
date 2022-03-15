650ba84b7a530ef010c9098376fe3d6ed9497227
kanaktyagi <tyagiknk.india@gmail.com>
2022-03-15 19:36:34 +0530

    Added the server side rendering for movies data

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,23 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import fetch from 'isomorphic-unfetch'
 
-export default function Home() {
+export default function Home({data}) {
+  console.log("data",data)
   return (
     <div className='container'>
       <h1> This is our front page</h1>    
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const {NEXT_PUBLIC_API_URL} = process.env
+  const res = await fetch(`${NEXT_PUBLIC_API_URL}/api/movies`)
+  const data = await res.json()
+   return {
+    props: {
+          data
+    }
+  }
 }
